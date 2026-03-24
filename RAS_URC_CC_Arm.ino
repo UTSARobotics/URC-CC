74d5ed861e4b747853d1ceca0a993a50988b43cf
Some-Dumb-Robot <ivan.garcia2754@gmail.com>
2026-03-24 16:37:21 -0500

    Add files via upload

--- RAS_URC_CC_Arm.ino
+++ RAS_URC_CC_Arm.ino
@@ -4,60 +4,130 @@
 // ---------------- PINS ----------------
 const byte JOINT1_PIN = 9;
 const byte JOINT2_PIN = 10;
-const byte LIFT_PIN = 11;
+const byte LIFT_PIN   = 11;
 
-// ---------------- ARM LENGTHS ----------------
-const float LINK1 = 100.0;
-const float LINK2 = 100.0;
+// ---------------- ARM LENGTHS (mm) ----------------
+const float LINK1 = 35.0;
+const float LINK2 = 42.0;
 
 // ---------------- POSITION ----------------
-float posX = 100;
-float posY = 0;
+float posX = 50;
+float posY = 20;
 
-// ---------------- SAFE LIMITS ----------------
+// ---------------- JOINT STATE ----------------
+int joint1Angle = 90;
+int joint2Angle = 90;
+int liftAngle   = 90;
+
+// ---------------- LIMITS ----------------
 const int JOINT1_MIN = 0;
 const int JOINT1_MAX = 180;
 const int JOINT2_MIN = 0;
 const int JOINT2_MAX = 180;
-const int LIFT_MIN = 0;
-const int LIFT_MAX = 180;
+const int LIFT_MIN   = 0;
+const int LIFT_MAX   = 180;
 
-// ---------------- HOME ----------------
-int joint1Angle = 90;
-int joint2Angle = 90;
-int liftAngle = 90;
-
-// movement step in mm
+// ---------------- MOTION SETTINGS ----------------
 const float STEP_SIZE = 10;
+const int INTERP_STEPS = 50;
+const int DELAY_MS = 40;
 
+// ---------------- SERVOS ----------------
 Servo joint1Servo;
 Servo joint2Servo;
 Servo liftServo;
 
-// ---------------- CLAMP ----------------
-int clampAngle(int angleValue, int minAngle, int maxAngle) {
-  if (angleValue < minAngle) return minAngle;
-  if (angleValue > maxAngle) return maxAngle;
-  return angleValue;
+// ---------------- UTILS ----------------
+int clampAngle(int v, int minA, int maxA) {
+  if (v < minA) return minA;
+  if (v > maxA) return maxA;
+  return v;
 }
 
-// ---------------- IK FUNCTION ----------------
-bool computeIK(float x, float y, int &theta1, int &theta2) {
+int angleDiff(int a, int b) {
+  int d = abs(a - b);
+  return min(d, 360 - d);
+}
+
+// ---------------- SMART IK ----------------
+bool computeIKSmart(float x, float y, int &theta1, int &theta2) {
   float distSq = x * x + y * y;
   float cosTheta2 = (distSq - LINK1 * LINK1 - LINK2 * LINK2) / (2 * LINK1 * LINK2);
 
-  // unreachable position
-  if (cosTheta2 < -1 || cosTheta2 > 1) {
+  if (cosTheta2 < -1 || cosTheta2 > 1) return false;
+
+  float t2a = acos(cosTheta2);
+  float t2b = -acos(cosTheta2);
+
+  float t1a = atan2(y, x) - atan2(LINK2 * sin(t2a), LINK1 + LINK2 * cos(t2a));
+  float t1b = atan2(y, x) - atan2(LINK2 * sin(t2b), LINK1 + LINK2 * cos(t2b));
+
+  int t1a_deg = degrees(t1a);
+  int t2a_deg = degrees(t2a);
+
+  int t1b_deg = degrees(t1b);
+  int t2b_deg = degrees(t2b);
+
+  int costA = angleDiff(t1a_deg, joint1Angle) + angleDiff(t2a_deg, joint2Angle);
+  int costB = angleDiff(t1b_deg, joint1Angle) + angleDiff(t2b_deg, joint2Angle);
+
+  if (costA <= costB) {
+    theta1 = t1a_deg;
+    theta2 = t2a_deg;
+  } else {
+    theta1 = t1b_deg;
+    theta2 = t2b_deg;
+  }
+
+  return true;
+}
+
+// ---------------- MOVE SINGLE POINT ----------------
+bool moveToXY(float x, float y) {
+  int t1, t2;
+
+  if (!computeIKSmart(x, y, t1, t2)) {
+    Serial.println(F("Target out of reach!"));
     return false;
   }
 
-  float theta2Rad = acos(cosTheta2);
-  float theta1Rad = atan2(y, x) - atan2(LINK2 * sin(theta2Rad), LINK1 + LINK2 * cos(theta2Rad));
+  joint1Angle = clampAngle(t1, JOINT1_MIN, JOINT1_MAX);
+  joint2Angle = clampAngle(t2, JOINT2_MIN, JOINT2_MAX);
 
-  theta1 = degrees(theta1Rad);
-  theta2 = degrees(theta2Rad);
+  joint1Servo.write(joint1Angle);
+  joint2Servo.write(joint2Angle);
+
+  posX = x;
+  posY = y;
 
   return true;
+}
+
+// ---------------- LINEAR INTERPOLATION ----------------
+void moveLinearXY(float targetX, float targetY) {
+  float startX = posX;
+  float startY = posY;
+
+  for (int i = 1; i <= INTERP_STEPS; i++) {
+    float t = (float)i / INTERP_STEPS;
+
+    float xi = startX + t * (targetX - startX);
+    float yi = startY + t * (targetY - startY);
+
+    moveToXY(xi, yi);
+    delay(DELAY_MS);
+  }
+}
+
+// ---------------- DRAW SQUARE ----------------
+void drawSquare(float size) {
+  float startX = posX;
+  float startY = posY;
+
+  moveLinearXY(startX + size, startY);
+  moveLinearXY(startX + size, startY + size);
+  moveLinearXY(startX, startY + size);
+  moveLinearXY(startX, startY);
 }
 
 // ---------------- PRINT ----------------
@@ -74,18 +144,6 @@ void printStatus() {
   Serial.println(liftAngle);
 }
 
-void printHelp() {
-  Serial.println();
-  Serial.println(F("SCARA IK CONTROL"));
-  Serial.println(F("----------------"));
-  Serial.println(F("w/s = Y up/down"));
-  Serial.println(F("a/d = X left/right"));
-  Serial.println(F("e/d = lift up/down"));
-  Serial.println(F("r = home"));
-  Serial.println(F("p = print status"));
-  Serial.println();
-}
-
 // ---------------- SETUP ----------------
 void setup() {
   Serial.begin(115200);
@@ -100,7 +158,6 @@ void setup() {
 
   delay(500);
 
-  printHelp();
   printStatus();
 }
 
@@ -109,41 +166,44 @@ void loop() {
   if (Serial.available() > 0) {
     char command = Serial.read();
 
-    // Move in XY space
-    if (command == 'w') posY += STEP_SIZE;
-    if (command == 's') posY -= STEP_SIZE;
-    if (command == 'a') posX -= STEP_SIZE;
-    if (command == 'd') posX += STEP_SIZE;
+    // -------- LINEAR MOVE --------
+    if (command == 'g') {
+      while (Serial.available() == 0);
+      float x = Serial.parseFloat();
 
-    // Lift
+      while (Serial.available() == 0);
+      float y = Serial.parseFloat();
+
+      Serial.print(F("Linear move to: "));
+      Serial.print(x);
+      Serial.print(F(", "));
+      Serial.println(y);
+
+      moveLinearXY(x, y);
+    }
+
+    // -------- MANUAL STEP --------
+    if (command == 'w') moveLinearXY(posX, posY + STEP_SIZE);
+    if (command == 's') moveLinearXY(posX, posY - STEP_SIZE);
+    if (command == 'a') moveLinearXY(posX - STEP_SIZE, posY);
+    if (command == 'd') moveLinearXY(posX + STEP_SIZE, posY);
+
+    // -------- SQUARE --------
+    if (command == 'q') drawSquare(20);
+
+    // -------- LIFT --------
     if (command == 'e') liftAngle += 5;
     if (command == 'c') liftAngle -= 5;
 
-    // Reset
+    // -------- RESET --------
     if (command == 'r') {
-      posX = 100;
-      posY = 0;
+      moveLinearXY(50, 20);
       liftAngle = 90;
-    }
-
-    // Compute IK
-    int t1, t2;
-    if (computeIK(posX, posY, t1, t2)) {
-      joint1Angle = clampAngle(t1, JOINT1_MIN, JOINT1_MAX);
-      joint2Angle = clampAngle(t2, JOINT2_MIN, JOINT2_MAX);
-
-      joint1Servo.write(joint1Angle);
-      joint2Servo.write(joint2Angle);
-    } else {
-      Serial.println(F("Target out of reach!"));
     }
 
     liftAngle = clampAngle(liftAngle, LIFT_MIN, LIFT_MAX);
     liftServo.write(liftAngle);
 
-    if (command == 'p' || command == 'w' || command == 'a' ||
-        command == 's' || command == 'd' || command == 'r') {
-      printStatus();
-    }
+    printStatus();
   }
 }
